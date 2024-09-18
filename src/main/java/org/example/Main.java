40b35a78f976a252e6826c5818bc4f62257d3115
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-09-17 20:33:39 -0600

    Modelo Estudiante y Prueba en el Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package main.java.org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import main.java.org.example.model.Estudiante;
 
-        System.out.println("Hola Mundo");
+import java.text.ParseException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+
+        Estudiante estudiante = new Estudiante(123456, 987654, "Juan", "Pérez", "2004/4/25", true);
+
+        System.out.println("CUI: " + estudiante.getCUI());
+        System.out.println("Carnet: " + estudiante.getCarnet());
+        System.out.println("Nombre: " + estudiante.getNombre());
+        System.out.println("Apellido: " + estudiante.getApellido());
+        System.out.println("Edad: " + estudiante.getEdad());
+        System.out.println("Fecha de Nacimiento: " + estudiante.getFechaDeNacimiento());
+        System.out.println("Estado: " + (estudiante.getEstado() ? "Activo" : "Inactivo"));
 
     }
 }
---
+++ src/main/java/org/example/model/Estudiante.java
@@ -0,0 +1,114 @@
+package main.java.org.example.model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Estudiante {
+
+    private final int CUI;
+    private final int carnet;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private Date fechaDeNacimiento;
+    private Boolean estado;
+
+    public Estudiante(int cui, int carnet, String nombre, String apellido, String fechaDeNacimientoString, Boolean estado) throws ParseException {
+        this.CUI = cui;
+        this.carnet = carnet;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = convertirStringADate(fechaDeNacimientoString);
+        this.edad = calcularEdad(convertirStringADate(fechaDeNacimientoString));
+        this.estado = estado;
+    }
+
+    public int getCUI() {
+        return CUI;
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimientoString) throws ParseException {
+
+        this.fechaDeNacimiento = convertirStringADate(fechaDeNacimientoString);
+
+        this.edad = calcularEdad(convertirStringADate(fechaDeNacimientoString));
+
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public static int calcularEdad(Date fechaDeNacimiento){
+
+        Calendar nacimiento = Calendar.getInstance();
+
+        nacimiento.setTime(fechaDeNacimiento);
+
+        Calendar actual = Calendar.getInstance();
+
+        int respuestaEdad = actual.get(Calendar.YEAR) - nacimiento.get(Calendar.YEAR);
+
+        System.out.println(respuestaEdad);
+
+        if (actual.get(Calendar.MONTH) < nacimiento.get(Calendar.MONTH) ||
+
+                (actual.get(Calendar.MONTH) == nacimiento.get(Calendar.MONTH) &&
+
+                        actual.get(Calendar.DAY_OF_MONTH) < nacimiento.get(Calendar.DAY_OF_MONTH))) {
+
+            respuestaEdad--;
+
+        }
+
+        return respuestaEdad;
+
+    }
+
+    public static Date convertirStringADate(String fechaString) throws ParseException {
+
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+
+        return formato.parse(fechaString);
+
+    }
+
+}
